963341e67c41da95349ec36cb5661b015e9b32a4
vtaylor84-vlt <vtaylor84@gmail.com>
2025-11-17 04:26:21 -0600

    Update vite.config.ts
    Fix Vite entry point for Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: 'index.html'
+      input: './index.html'  // Fixes entry point resolution
     }
   }
 });
